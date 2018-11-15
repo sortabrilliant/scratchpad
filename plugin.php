9cec48f31fbf3fc075dacd27cbcaac500d90b6e6
JR Tashjian <jrtashjian@gmail.com>
2018-11-15 13:58:59 -0500

    Bumping version to 1.0.1

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Keep notes where they're needed most.
  * Author: sorta brilliant
  * Author URI: https://sortabrilliant.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
