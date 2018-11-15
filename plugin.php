fe3fb0e1cdb0c0e359f6c152b61dfd2f691c6049
JR Tashjian <jrtashjian@gmail.com>
2018-11-15 13:52:21 -0500

    Renaming the GitHub Updater class to avoid naming collisions.

--- plugin.php
+++ plugin.php
@@ -41,7 +41,7 @@ function sbb_scratchpad_github_plugin_updater_test_init() {
 			'readme'             => 'README.md',
 		);
 
-		new WP_GitHub_Updater( $config );
+		new SBB_Scratchpad_GitHub_Updater( $config );
 	}
 }
 add_action( 'init', 'sbb_scratchpad_github_plugin_updater_test_init' );
